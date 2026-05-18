54fbb315a126287837b3480731815b6d7cc903ea
olroX-cyber <olrox85@gmail.com>
2026-05-18 10:22:16 +0200

    Add support for IP65 Outdoor Thermo-Hygrometer (WoIOSensor) in overview (#52)
    * Update deviceDefinitions.ts
    
    Add support for IP65 Outdoor Thermo-Hygrometer (WoIOSensor) in overview
    
    * Update deviceDefinitions.ts
    
    Fix syntax error in meter matchers
    
    * Update deviceDefinitions.ts
    
    Fix extra closing bracke.
    
    * Update deviceDefinitions.ts
    
    (Clean up meter matchers - keep only official woiosensor

--- src/renderer/src/deviceDefinitions.ts
+++ src/renderer/src/deviceDefinitions.ts
@@ -751,7 +751,7 @@ const definitions: DeviceDefinition[] = [
   },
   {
     key: "meter",
-    matchers: ["meter plus", "meter pro co2", "meter pro", "outdoor meter", "meter"],
+    matchers: ["meter plus", "meter pro co2", "meter pro", "outdoor meter", "meter", "woiosensor"],
     statusFields: [
       ...tempHumidityFields,
       {
