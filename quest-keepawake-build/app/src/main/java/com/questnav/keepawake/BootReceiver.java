e0951566f03afce74d2f6beeb858b29039eb6442
suu <46421931+0suu@users.noreply.github.com>
2026-07-19 22:10:35 +0900

    ci: add boot receiver

---
+++ quest-keepawake-build/app/src/main/java/com/questnav/keepawake/BootReceiver.java
@@ -0,0 +1,3 @@
+package com.questnav.keepawake;
+import android.content.BroadcastReceiver; import android.content.Context; import android.content.Intent;
+public class BootReceiver extends BroadcastReceiver { @Override public void onReceive(Context c, Intent i){ c.startForegroundService(new Intent(c, KeepAwakeService.class)); } }
